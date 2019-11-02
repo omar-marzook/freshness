c762ef1ac0c032db7634f260d2056a13a023e3f6
omar-marzook <omarmarzook92@gmail.com>
2019-11-02 21:32:22 +0200

    Add Dark Theme

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header.js";
 import HeroSlider from "./components/HeroSlider.js";
 import IntroCard from "./components/IntroCard.js";
@@ -6,50 +6,108 @@ import ServicesCard from "./components/ServicesCard.js";
 import Footer from "./components/Footer.js";
 import Copyrights from "./components/Copyrights.js";
 
-function App() {
-  return (
-    <main className="app">
-      <Header />
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      checked: localStorage.getItem("theme") === "dark" ? true : false,
+      theme: localStorage.getItem("theme")
+    };
+  }
 
-      <section className="container silder_section">
-        <HeroSlider />
-      </section>
+  componentDidMount() {
+    document
+      .getElementsByTagName("HTML")[0]
+      .setAttribute("theme", localStorage.getItem("theme"));
+  }
 
-      <section className="container intro_section">
-        <IntroCard />
-        <IntroCard />
-        <IntroCard />
-      </section>
+  toggleThemeChange() {
+    const { checked } = this.state;
+    if (checked === false) {
+      document.documentElement.classList.add("theme-transition");
 
-      <article className="container shout" tabIndex="0">
-        <h3 className="shout-text">
-          “ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non
-          diam erat. In fringilla massa ut nisi ullamcorper pellentesque „
-        </h3>
-      </article>
+      localStorage.setItem("theme", "dark");
+      document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: true
+      });
+      window.setTimeout(function() {
+        document.documentElement.classList.remove("theme-transition");
+      }, 700);
+    } else {
+      document.documentElement.classList.add("theme-transition");
 
-      <section className="container services_section">
-        <ServicesCard />
-        <ServicesCard />
-        <ServicesCard />
-        <ServicesCard />
-        <ServicesCard />
-        <ServicesCard />
-      </section>
+      localStorage.setItem("theme", "light");
+      document
+        .getElementsByTagName("HTML")[0]
+        .setAttribute("theme", localStorage.getItem("theme"));
+      this.setState({
+        checked: false
+      });
+      window.setTimeout(function() {
+        document.documentElement.classList.remove("theme-transition");
+      }, 700);
+    }
+  }
 
-      <footer className="footer" role="contentinfo" tabIndex="0">
-        <div className="container">
-          <Footer />
+  render() {
+    return (
+      <main className="app">
+        <div className="theme_color">
+          <p>Change Theme Color</p>
+          <label className="switch">
+            <input
+              type="checkbox"
+              defaultChecked={this.state.checked}
+              onChange={() => this.toggleThemeChange()}
+            />
+            <span className="switcher round"></span>
+          </label>
         </div>
-      </footer>
+        <Header />
 
-      <footer className="copyrights" tabIndex="0">
-        <div className="container">
-          <Copyrights />
-        </div>
-      </footer>
-    </main>
-  );
+        <section className="container slider_section">
+          <HeroSlider />
+        </section>
+
+        <section className="container intro_section">
+          <IntroCard />
+          <IntroCard />
+          <IntroCard />
+        </section>
+
+        <article className="container shout" tabIndex="0">
+          <h3 className="shout-text">
+            “ Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non
+            diam erat. In fringilla massa ut nisi ullamcorper pellentesque „
+          </h3>
+        </article>
+
+        <section className="container services_section">
+          <ServicesCard />
+          <ServicesCard />
+          <ServicesCard />
+          <ServicesCard />
+          <ServicesCard />
+          <ServicesCard />
+        </section>
+
+        <footer className="footer" role="contentinfo" tabIndex="0">
+          <div className="container">
+            <Footer />
+          </div>
+        </footer>
+
+        <footer className="copyrights" tabIndex="0">
+          <div className="container">
+            <Copyrights />
+          </div>
+        </footer>
+      </main>
+    );
+  }
 }
 
 export default App;
